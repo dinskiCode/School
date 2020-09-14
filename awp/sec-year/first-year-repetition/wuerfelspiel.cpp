e90fb5e42af2769003a688b91b6a367d0f36da87
dinskiCode <wmatthi96@gmail.com>
2020-09-14 20:39:58 +0200

    introduction to structures

--- awp/sec-year/first-year-repetition/wuerfelspiel.cpp
+++ awp/sec-year/first-year-repetition/wuerfelspiel.cpp
@@ -1,17 +1,79 @@
 #include <iostream>
 #include <time.h>
+#include <string>
 
 using namespace std;
 
-//
+// Funktionen
 int wuerfle();
 
+//Datenstrukturen
+struct t_wuerfelspiel {
+    string name;
+    int ergebnis[10]; 
+};
+
 int main() {
-    cout << "\n\t\t\tDas Würfelspiel\n\n";
+    cout << "\n\t\t--- Das Würfelspiel ---\n\n";
     srand((unsigned)time(NULL));
-    int spieler = 8; 
-    int wuerfe = 10;
-    int ergebnisse[spieler][wuerfe]; 
+
+    const int spieler = 8;
+    const int wuerfe = 10;
+    t_wuerfelspiel mitspieler[spieler];
+    string namen[spieler] = {"Hans", "Petra", "Lena", "Fritz", "Horst", "Michi", "Anna", "Peter"};
+    int hauefigkeiten[6] = {0};
+
+    for (int i = 0; i < spieler; i++) {
+        mitspieler[i].name = namen[i];
+        cout << mitspieler[i].name <<":\t";
+        for (int k = 0; k < wuerfe; k++) {
+            mitspieler[i].ergebnis[k] = wuerfle();
+            cout << mitspieler[i].ergebnis[k] << "  ";
+        }
+        cout << endl;
+    }
+
+    // Haeufigkeiten
+    for (int i = 0; i < spieler; i++) {
+        for (int k = 0; k < wuerfe; k++) {
+            switch (mitspieler[i].ergebnis[k])
+            {
+            case 1:
+                hauefigkeiten[0] += 1;
+                break;
+            case 2:
+                hauefigkeiten[1] += 1;
+                break;
+            case 3:
+                hauefigkeiten[2] += 1;
+                break;
+            case 4:
+                hauefigkeiten[3] += 1;
+                break;
+            case 5:
+                hauefigkeiten[4] += 1;
+                break;
+            case 6:
+                hauefigkeiten[5] += 1;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+    cout << "\nZiehungshaeufigkeiten der Zahlen 1-6:" << endl;
+    for (int i = 0; i < 6; i++) {
+        cout << "Die Zahl " << i+1 << ": " << hauefigkeiten[i] << " mal" << endl;
+    }
+    
+
+    /*
+    --- OLD ---
+    const int spieler = 8; 
+    const int wuerfe = 10;
+    int ergebnisse[spieler][wuerfe] = {0};
+    int hauefigkeiten[6] = {0}; 
 
     for (int i = 0; i < spieler; i++) {
         cout << "Spieler " << i+1 << ":\t";
@@ -23,7 +85,6 @@ int main() {
     }
 
     // Haeufigkeiten
-    int hauefigkeiten[6] = {0};
     for (int i = 0; i < spieler; i++) {
         for (int k = 0; k < wuerfe; k++) {
             switch (ergebnisse[i][k])
@@ -55,7 +116,7 @@ int main() {
     cout << "\nZiehungshaeufigkeiten der Zahlen 1-6:" << endl;
     for (int i = 0; i < 6; i++) {
         cout << "Die Zahl " << i+1 << ": " << hauefigkeiten[i] << " mal" << endl;
-    }
+    }*/
     cout << endl;
 
     return 1;
