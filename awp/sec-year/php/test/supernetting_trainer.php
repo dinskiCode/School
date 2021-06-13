a9a8cc8894194434f67829d5089a0781ce2571f4
dinskiCode <wmatthi96@gmail.com>
2021-06-13 18:14:10 +0200

    small change

--- awp/sec-year/php/test/supernetting_trainer.php
+++ awp/sec-year/php/test/supernetting_trainer.php
@@ -4,20 +4,20 @@
     </head>
     <body>
         <?php
-            $amount_of_ips = 3; // how many ips are going to be generated 
-            $ips = array('', '', '');
-            $octets_equal = 1;
+            $amount_of_ips = 4; // how many ips are going to be generated 
+            $ips = array(); // no need for initializing arrays, can even access by index that doesnt exist yet apparently
+            $octets_equal = 2;
 
             // generate equal octets
             for ($i = 0; $i < $octets_equal; $i++) {
                 $equal_octet = mt_rand(0, 255);
-                for ($k = 0; $k < sizeof($ips); $k++) {
+                for ($k = 0; $k < $amount_of_ips; $k++) {
                     $ips[$k] .= "$equal_octet.";
                 }
             }
             
             // generate the rest of the ip
-            for ($i = 0; $i < 3; $i++) {
+            for ($i = 0; $i < $amount_of_ips; $i++) {
                 $ip = '';
                 $subnet = mt_rand(10, 30);
                 for ($k = 0; $k < 4 - $octets_equal; $k++) {
@@ -25,12 +25,14 @@
                     if ($k < 3 - $octets_equal) {
                         $ip .= "$num.";    
                     } else {
-                        $ip .= "$num";
+                        $ip .= "0";
                     }
                 }
                 $ip .= "/$subnet";
                 $ips[$i] .= $ip;
-            } 
+            }
+
+            // print ips of ip array 
             foreach($ips as $ip) {
                 echo "<br><span style='font-size: 1.5rem'>$ip</span></br>";
             }
