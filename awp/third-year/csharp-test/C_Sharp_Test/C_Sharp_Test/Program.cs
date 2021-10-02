eaa23385138cfff553ac75222ff3548d765524cb
dinskiCode <wmatthi96@gmail.com>
2021-10-02 17:13:58 +0200

    c# test using visual studio for mac

---
+++ awp/third-year/csharp-test/C_Sharp_Test/C_Sharp_Test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C_Sharp_Test
+{
+    class Program
+    {
+        String name;
+        int yearOfBuild;
+        double price;
+
+        public Program(String name, int yearOfBuild, double price)
+        {
+            this.name = name;
+            this.yearOfBuild = yearOfBuild;
+            this.price = price;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Program consoleProgram = new Program("Consoleprogram", 2021, 12.99);
+            Console.WriteLine(consoleProgram.name, " = program name");
+        }
+    }
+}
